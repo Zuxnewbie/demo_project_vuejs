dba1276355a2f39452fa754b73c524a31cfcafcd
Huynh Thanh Nhan <huynhthanhnhan632004@gmail.com>
2024-03-29 23:40:19 +0700

    Change checkOut

--- vue_/src/views/CheckOutView.vue
+++ vue_/src/views/CheckOutView.vue
@@ -17,11 +17,7 @@
 
             <div class="form-item">
               <label class="form-label my-3">Address <sup>*</sup></label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="House Number Street Name"
-              />
+              <input type="text" class="form-control" placeholder="House Number Street Name" />
             </div>
 
             <div class="form-item">
@@ -30,14 +26,8 @@
             </div>
             <hr />
             <div class="form-item">
-              <textarea
-                name="text"
-                class="form-control"
-                spellcheck="false"
-                cols="30"
-                rows="11"
-                placeholder="Oreder Notes (Optional)"
-              ></textarea>
+              <textarea name="text" class="form-control" spellcheck="false" cols="30" rows="11"
+                placeholder="Oreder Notes (Optional)"></textarea>
             </div>
           </div>
           <div class="col-md-12 col-lg-6 col-xl-5">
@@ -53,71 +43,44 @@
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
+                  <tr v-for="product in carts" :key="product.id">
                     <th scope="row">
                       <div class="d-flex align-items-center mt-2">
-                        <img
-                          src="../assets/Frontend/img/vegetable-item-2.jpg"
-                          class="img-fluid rounded-circle"
-                          style="width: 90px; height: 90px"
-                          alt=""
-                        />
+                        <img :src="product.imageURL" class="img-fluid rounded-circle" style="width: 90px; height: 90px"
+                          alt="" />
                       </div>
                     </th>
-                    <td class="py-5">Awesome Brocoli</td>
-                    <td class="py-5">$69.00</td>
-                    <td class="py-5">2</td>
-                    <td class="py-5">$138.00</td>
+                    <td class="py-5">{{ product.productName }}</td>
+                    <td class="py-5">{{ product.price }}</td>
+                    <td class="py-5">{{ product.quantity }}</td>
+                    <td class="py-5">{{ product.price }} đ</td>
                   </tr>
                 </tbody>
               </table>
             </div>
 
-            <div
-              class="row g-4 text-center align-items-center justify-content-center border-bottom py-3"
-            >
+            <div class="row g-4 text-center align-items-center justify-content-center border-bottom py-3">
               <div class="col-12">
                 <div class="form-check text-start my-3">
-                  <input
-                    type="checkbox"
-                    class="form-check-input bg-primary border-0"
-                    id="Payments-1"
-                    name="Payments"
-                    value="Payments"
-                  />
-                  <label class="form-check-label" for="Payments-1"
-                    >Check Payments</label
-                  >
+                  <input type="checkbox" class="form-check-input bg-primary border-0" id="Payments-1" name="Payments"
+                    value="Payments" />
+                  <label class="form-check-label" for="Payments-1">Check Payments</label>
                 </div>
               </div>
               <div>?????????????????</div>
             </div>
-            <div
-              class="row g-4 text-center align-items-center justify-content-center border-bottom py-3"
-            >
+            <div class="row g-4 text-center align-items-center justify-content-center border-bottom py-3">
               <div class="col-12">
                 <div class="form-check text-start my-3">
-                  <input
-                    type="checkbox"
-                    class="form-check-input bg-primary border-0"
-                    id="Delivery-1"
-                    name="Delivery"
-                    value="Delivery"
-                  />
-                  <label class="form-check-label" for="Delivery-1"
-                    >Cash On Delivery</label
-                  >
+                  <input type="checkbox" class="form-check-input bg-primary border-0" id="Delivery-1" name="Delivery"
+                    value="Delivery" />
+                  <label class="form-check-label" for="Delivery-1">Cash On Delivery</label>
                 </div>
               </div>
             </div>
 
-            <div
-              class="row g-4 text-center align-items-center justify-content-center pt-4"
-            >
-              <button
-                type="button"
-                class="btn border-secondary py-3 px-4 text-uppercase w-100 text-primary"
-              >
+            <div class="row g-4 text-center align-items-center justify-content-center pt-4">
+              <button type="button" class="btn border-secondary py-3 px-4 text-uppercase w-100 text-primary">
                 Place Order
               </button>
             </div>
@@ -131,8 +94,10 @@
 
 <script>
 export default {
+  props: ["baseURL", "carts"],
+
   name: "CheckOutView",
-  data() {},
+  data() { },
 };
 </script>
 
